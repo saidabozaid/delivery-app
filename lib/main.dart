b8976cc7b4d732fbc8e33e321eeb4648bfaf03a4
saidabozaid <142732390+saidabozaid@users.noreply.github.com>
2026-09-07 23:36:49 +0300

    Change Supabase anonKey in main.dart
    Updated Supabase anonKey for initialization.

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Supabase.initialize(
     url: 'https://flszwmgtkiysvxpvzgtb.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZsc3p3bWd0a2l5c3Z4cHZ6Z3RiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU3OTM2OTAsImV4cCI6MjA0MTM2OTY5MH0.6Q7R5eJx_example_replace_with_real_key',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZsc3p3bWd0a2l5c3Z4cHZ6Z3RiIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4ODcyMzI3MCwiZXhwIjoyMTA0Mjk5MjcwfQ.xAsPgzCR60Fp9Pxuw2iM9MXN_sM6vUvAqRRuAfQ6I-E',
   );
   runApp(DeliveryShopApp());
 }
