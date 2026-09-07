2a58d6033f65478830904e2d83874f0693e0fcf8
saidabozaid <142732390+saidabozaid@users.noreply.github.com>
2026-09-08 00:12:47 +0300

    Fix import statement for Supabase Flutter package

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:supabase_flutter/supabase.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
